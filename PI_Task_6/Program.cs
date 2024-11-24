efd9b818e0edce859d496e561aa4df516144e700
Млынник Семен <146517575+Mlynnik@users.noreply.github.com>
2024-11-24 15:14:02 +0300

    create_program

---
+++ PI_Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PI_Task_6
+{
+    internal class Program
+    {
+        static int sum_number(int a, int b) { return a + b; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum_number(1, 2));
+        }
+    }
+}
