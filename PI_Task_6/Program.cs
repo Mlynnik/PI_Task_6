7f654171c6df0885da2da3f36943d678252e1924
Млынник Семен <146517575+Mlynnik@users.noreply.github.com>
2024-11-30 14:10:54 +0300

    failed_2

--- PI_Task_6/Program.cs
+++ PI_Task_6/Program.cs
@@ -3,7 +3,7 @@
     public class task6
     {
         public static int sum_number(int a, int b) { return a + b; }
-
+        не компилься
         static void Main(string[] args)
         {
             Console.WriteLine(sum_number(1, 2));
